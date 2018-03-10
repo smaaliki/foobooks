3f0747dd2fb03da2ca3f44eed2bddf5a5162ca4f
Samer Maaliki <smaaliki@hotmail.com>
2018-03-10 19:08:14 +0400

    Controllers and web.php were not checked in

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Config;
+
+class PageController extends Controller
+{
+    public function welcome()
+    {
+        return view('welcome');
+    }
+
+    public function about()
+    {
+        return 'About Us';
+    }
+
+    public function contact()
+    {
+        return 'Questions? Email us at ' . Config::get('app.supportEmail');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'PageController@welcome');
+Route::get('/about', 'PageController@about');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/contact', 'PageController@contact');
 
-Route::get('/example', function () {
-    return 'hello there!';
-});
+Route::get('/books', 'BookController@index');
+Route::get('/books/{title?}', 'BookController@show');
 
-Route::get('/xyz', function () {
-    return view('xyz');
-});
+/**
+ * Practice
+ */
+Route::any('/practice/{n?}', 'PracticeController@index');
