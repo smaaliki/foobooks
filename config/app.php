6800e4513f751bb7e4c69218f459480e888dc00f
Samer Maaliki <smaaliki@hotmail.com>
2018-03-10 17:56:44 +0400

    Changes from config/env lecture and packages lecture

--- config/app.php
+++ config/app.php
@@ -2,6 +2,7 @@
 
 return [
 
+    'supportEmail' => 'smaaliki@hotmail.com',
     /*
     |--------------------------------------------------------------------------
     | Application Name
@@ -13,7 +14,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => 'Foobooks',
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +53,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://foobooks.ulaila.com'),
 
     /*
     |--------------------------------------------------------------------------
